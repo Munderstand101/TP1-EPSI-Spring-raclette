84edb9c4025f369694c6da59f9f6a79fce8e7eb9
Munderstand101 <asukiller101@gmail.com>
2023-02-21 08:42:03 +0100

    Initial commit

---
+++ src/main/java/dev/epsi/raclette/RacletteApplication.java
@@ -0,0 +1,13 @@
+package dev.epsi.raclette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/epsi/raclette/RacletteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.epsi.raclette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
