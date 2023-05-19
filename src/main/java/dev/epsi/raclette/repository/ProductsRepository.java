5764062138d3ad6fdf3a4b2ee6a6965bafcb3519
Munderstand101 <asukiller101@gmail.com>
2023-05-19 15:18:03 +0200

    ...

--- src/main/java/dev/epsi/raclette/repository/ProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.epsi.raclette.repository;
-
-
-import dev.epsi.raclette.entity.Products;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductsRepository extends CrudRepository<Products,Long > {
-}
